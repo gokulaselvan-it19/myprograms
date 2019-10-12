81460d0816ef37ad7a2324c8a35a99182ba93cb3
user.name <gokulaselvan.it19@bitsathy.ac.in>
2019-10-12 14:12:36 +0530

    first commit

---
+++ prog11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int i,j,a[10],n;
+    printf("Enter a ten number:");
+    for(i=0;i<10;i++)
+    scanf("%d",&a[n]);
+    {
+    for(j=0;j<10;j++)
+    {
+        printf("Addres of %d is %p\n",j);
+    }
+}
+    return 0;
+}
