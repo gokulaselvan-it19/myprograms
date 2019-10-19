eec6f83a93f44486b3a24d7f0929c6db85b69004
user.name <gokulaselvan.it19@bitsathy.ac.in>
2019-10-19 11:38:03 +0530

    first commit

---
+++ prog2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int  main()
+{
+ int i,fact=1,n;
+ printf("enter a number for factorial:\n");
+ scanf("%d",&n);
+ for(i=1;i<=n;i++)
+
+  fact=fact*i;
+
+printf("the factorial of %d is %d",n,fact);
+return 0;
+}
+
