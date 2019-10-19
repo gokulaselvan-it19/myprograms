bbf8de5328bdc24bf547f9232be69c73dc27ec9f
user.name <gokulaselvan.it19@bitsathy.ac.in>
2019-10-19 11:42:41 +0530

    first commit

---
+++ prog47.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int l,w,area;
+printf("Enter the length of rectangle:");
+scanf("%d",&l);
+printf("Enter the width of rectangle:");
+scanf("%d",&w);
+{
+area=l*w;
+}
+printf("the area of rectangle: %d",area);
+return 0;
+}
+
